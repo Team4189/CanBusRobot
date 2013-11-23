763a2bed6cce1802d0c4b8ae6b5e97caaa18520f
hal <hal@hal-asus>
2013-11-23 09:53:48 -0500

    Created Gripper

---
+++ src/org/team4189/code2013/CanChasisRobot/subsystems/Gripper.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.team4189.code2013.CanChasisRobot.subsystems;
+
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Relay;
+/**
+ *
+ * @author hal
+ */
+public class Gripper extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
